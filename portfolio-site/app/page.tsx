c8a3be8aef7bc77f0900d8b7d4dafe92d27c1d49
Fadel Bantan <fadooly12@gmail.com>
2025-06-03 21:22:45 +0100

    SystemArchitecture uncommented

--- portfolio-site/app/page.tsx
+++ portfolio-site/app/page.tsx
@@ -9,7 +9,7 @@ export default function BackendPortfolio() {
 	return (
 		<main className="min-h-screen bg-[#1A1A1A] text-white overflow-x-hidden">
 			<HeroSection />
-			{/* <SystemArchitecture /> */}
+			<SystemArchitecture />
 			{/* <TechnicalMetrics /> */}
 			<ContactSection />
 		</main>
