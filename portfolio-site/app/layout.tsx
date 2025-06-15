b24c43cd39151c6f39de30e06f176dda540fbd62
Fadel Bantan <fadooly12@gmail.com>
2025-06-15 16:15:01 +0100

    Remove unused ContactSection import

--- portfolio-site/app/layout.tsx
+++ portfolio-site/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/NavBar';
-import ContactSection from './components/ContactSection';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
