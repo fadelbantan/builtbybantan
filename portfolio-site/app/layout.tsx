d940066d464a36bb920d14bdae8f588370e05447
Fadel Bantan <fadooly12@gmail.com>
2025-06-15 15:50:28 +0100

    Added ContactSection.tsx back and fixed imports

--- portfolio-site/app/layout.tsx
+++ portfolio-site/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
+import ContactSection from './components/ContactSection';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
